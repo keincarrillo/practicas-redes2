0d2d291f4aaec08dcf3adbf09b33020bc39e8636
keincarrillo <kein.carrillo.escom@gmail.com>
2025-10-03 14:06:23 -0600

    refactor

--- practica1/fullstack/server/src/proxy/index.js
+++ practica1/fullstack/server/src/proxy/index.js
@@ -3,5 +3,5 @@ import app from './src/app.js'
 const port = process.env.PORT_PROXY || 3000
 
 app.listen(port, () => {
-  console.log('Server running on port 3000')
+  console.log(`Proxy server listening on port ${port}`)
 })
